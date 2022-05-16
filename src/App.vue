e2207d17883911d72bd63e8bb5f5675091a96ada
Noopur Phalak <noopur@pixeldust.in>
2022-05-16 12:28:27 +0530

    Number Animation through gsap

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AnimatedNumber from "@/components/AnimatedNumber.vue";
+import { onMounted, ref } from "vue";
+
+const number = ref(0);
+
+onMounted(() => {
+  number.value = 10;
+});
+</script>
+
+<template>
+  <AnimatedNumber :number="number" />
+</template>
+
+<style></style>
---
+++ src/components/AnimatedNumber.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ Intl.NumberFormat("en-IN").format(tweened.number.toFixed(0)) }}</div>
+</template>
+
+<script setup lang="ts">
+import gsap from "gsap";
+import { reactive, watch } from "vue";
+const props = defineProps({ number: Number });
+const tweened = reactive({ number: 0 });
+watch(
+  () => props.number,
+  (value) => {
+    gsap.to(tweened, { duration: 1, number: Number(value) || 0 });
+  }
+);
+</script>
